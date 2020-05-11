e797972c1a9606374990061cd2168038f6acfcfc
Javier Ros Moreno <jros@jros.org>
2020-05-11 11:02:08 +0200

    fix: remove useless AmbientDbContextConfiguration

--- Jros.AmbientDbContext/Microsoft/Extensions/DependencyInjection/AmbientDbContextServiceCollectionExtensions.cs
+++ Jros.AmbientDbContext/Microsoft/Extensions/DependencyInjection/AmbientDbContextServiceCollectionExtensions.cs
@@ -1,23 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Jros.AmbientDbContext;
+﻿using Jros.AmbientDbContext;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class AmbientDbContextServiceCollectionExtensions
     {
-        public static IServiceCollection AddAmbientDbContext(this IServiceCollection sc, Action<AmbientDbContextConfiguration> configureAction = null)
+        public static IServiceCollection AddAmbientDbContext(this IServiceCollection sc)
         {
             sc.AddSingleton<IDbContextScopeFactory, DbContextScopeFactory>();
             sc.AddTransient<IAmbientDbContextLocator, AmbientDbContextLocator>();
             sc.AddTransient<IDbContextFactory, ServiceProviderDbContextFactory>();
-
-            var configuration = new AmbientDbContextConfiguration(sc);
-
-            sc.AddSingleton(configuration);
-
-            configureAction?.Invoke(configuration);
 
             return sc;
         }
